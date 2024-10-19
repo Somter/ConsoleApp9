b22d24eca28c79c90c6d25b72a774b8466efddd9
Sasha <alecsand.unit@gmail.com>
2024-10-19 22:47:14 +0300

    dz

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -7,27 +7,7 @@ class MainClass
     {
         try
         {
-            //Person person = new Person("Иван", "Иванов", "123-456", 30);    
-            ////person.Print();
-
-            //Console.WriteLine();
-
-            //Student student = new Student("Анна", "Смирнова", 20, "789-101", 5, 101);
-            ////student.Print();
-            //Student student2 = new Student("Anna", "lolol", 20, "789-101", 4.5, 5);
-
-            //Academy_Group academy_Group = new Academy_Group();
-            //academy_Group.Add(student);
-            //academy_Group.Add(student2);
-            ////academy_Group.Remove("Смирнова");
-            ////academy_Group.Edit("Смирнова", student2);
-            ////academy_Group.Print();
-            //academy_Group.Save();
-            //academy_Group.Load();
-            //academy_Group.Print();
-            //academy_Group.Search(6);    
-
-            ClassMenu menu = new ClassMenu();
+            ClassMenu menu = new ClassMenu();   
             menu.Menu();    
         }
         catch (Exception ex) 
