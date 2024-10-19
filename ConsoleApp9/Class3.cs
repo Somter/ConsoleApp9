ac1f41ade72b655cbf6b5fe9b29178be61ca42f6
Sasha <alecsand.unit@gmail.com>
2024-10-19 20:44:19 +0300

    Add project files.

---
+++ ConsoleApp9/Class3.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;    
+
+namespace ConsoleApp9
+{
+    class Academy_Group
+    {
+        ArrayList students;
+        int count;
+
+        public Academy_Group() 
+        {
+            students = new ArrayList();
+            count = 0;  
+        }
+
+        // Метод добавляет студентов в группу 
+        public void Add(Student student)     
+        {
+            students.Add(student);
+            count++;
+            Console.WriteLine("Студент " + student.Name + " " + student.Surname + " добавлен");         
+           
+        }
+
+        //Метод удаляет студентов из группы
+        public void Remove(string student_surname)
+        {
+            for (int i = 0; i < students.Count; i++)
+            {
+                Student student = (Student)students[i];
+                if (student.Surname.Equals(student_surname, StringComparison.OrdinalIgnoreCase))
+                {
+                    students.RemoveAt(i);  
+                    count--;
+                    Console.WriteLine("Студент " + student_surname + " удалён из группы");
+                    return;
+                }
+            }
+            
+            Console.WriteLine("Студент " + student_surname + " не найден в группе.");
+        }
+
+
+        // Метод позволяет редактировать студента
+        public void Edit(string student_surname, Student NewStudent)    
+        {
+            for (int i = 0; i < students.Count; i++) 
+            {
+                Student student = (Student)students[i];
+                if (student.Surname == student_surname)
+                {
+                    students[i] = NewStudent;
+                    Console.WriteLine("Студент " + student_surname + " редактирован на студента " + NewStudent.Surname);
+                    return;
+                }
+               
+            }
+        }
+
+       // Выводим всю группу 
+       public void Print() 
+       {
+          for (int i = 0; i < students.Count; i++) 
+          {
+             Student student = (Student)students[i];
+             Console.WriteLine("\n");
+             student.Print();              
+          }
+       }
+
+        // Сохраняем группу в файл
+        public void Save() 
+        {
+            using (StreamWriter write_to_file = new StreamWriter("Group.txt", false)) 
+            {
+                for (int i = 0; i < students.Count; i++)
+                {
+                    Student student = (Student)students[i];
+                    string line = "Имя - " + student.Name + "\nSurname - " + student.Surname + "\nPhone - " + student.Phone + "\nAge - " + student.Age + "\nAverage - " + student.Average + "\nNumber of group - " + student.NumberOfGroup;
+
+                    write_to_file.WriteLine(line);
+                    write_to_file.WriteLine();
+                }
+            }
+            Console.WriteLine("Данные о студентах сохранены в файл");
+        }
+
+        // Загружаем данные из файла
+        public void Load()
+        {
+            students.Clear();
+            count = 0;
+
+            using (StreamReader reade_file = new StreamReader("Group.txt"))
+            {
+                string nameLine;
+                while ((nameLine = reade_file.ReadLine()) != null)
+                {
+                    string surnameLine = reade_file.ReadLine();
+                    string phoneLine = reade_file.ReadLine();
+                    string ageLine = reade_file.ReadLine();
+                    string averageLine = reade_file.ReadLine();
+                    string groupLine = reade_file.ReadLine();
+
+                    reade_file.ReadLine();
+
+                    string name = nameLine.Split('-')[1].Trim();
+                    string surname = surnameLine.Split('-')[1].Trim();
+                    string phone = phoneLine.Split('-')[1].Trim();
+                    int age = int.Parse(ageLine.Split('-')[1].Trim());
+                    double average = double.Parse(averageLine.Split('-')[1].Trim());
+                    int numberOfGroup = int.Parse(groupLine.Split('-')[1].Trim());
+
+                    Student student = new Student(name, surname, age, phone, average, numberOfGroup);
+                    students.Add(student);
+                    count++;
+                }
+            }
+
+            Console.WriteLine("Данные о студентах загружены из файла.");
+        }
+
+        // метод для поиска студентов по заданному критерию
+        // Equals - метод сравнивет строки
+        public void Search(int criterion_number) 
+        {
+            bool found = false;
+            switch (criterion_number){
+                case 1:
+                   
+                    Console.Write("Введите имя для поиска: ");
+                    string search_name = Console.ReadLine();
+                    foreach (Student student in students) 
+                    {
+                        if (student.Name.Equals(search_name, StringComparison.OrdinalIgnoreCase)) 
+                        {
+                            Console.WriteLine("Такой студент есть:");
+                            Console.WriteLine("\n");    
+                            student.Print();
+                            found = true;
+                        }      
+                    }
+        
+                    if (!found)
+                    {
+                        Console.WriteLine("Такого студента нет.");
+                    }
+                    break;
+                case 2:
+                   
+                    Console.Write("Введите фамилию для поиска: ");
+                    string search_surname = Console.ReadLine();
+                    foreach (Student student in students)
+                    {
+                        if (student.Surname.Equals(search_surname, StringComparison.OrdinalIgnoreCase))
+                        {
+                            Console.WriteLine("Такой студент есть: ");
+                            student.Print();
+                            found = true;
+                        }
+                    }
+
+                    if (!found)
+                    {
+                        Console.WriteLine("Такого студента нет.");
+                    }
+                    break;
+                case 3:
+                   
+                    Console.Write("Введите телефон для поиска: ");
+                    string search_phone = Console.ReadLine();
+                    foreach (Student student in students)
+                    {
+                        if (student.Phone.Equals(search_phone, StringComparison.OrdinalIgnoreCase))
+                        {
+                            Console.WriteLine("Такой студент есть: ");
+                            student.Print();
+                            found = true;
+                        }
+                    }
+                  
+                    if (!found)
+                    {
+                        Console.WriteLine("Такого студента нет.");
+                    }
+                    break;
+                case 4:
+                 
+                    Console.Write("Введите возраст для поиска: ");
+                    string search_age = Console.ReadLine();
+                    foreach (Student student in students)
+                    {
+                        string str_age = student.Age.ToString();           
+                        if (str_age.Equals(search_age, StringComparison.OrdinalIgnoreCase))
+                        {
+                            Console.WriteLine("Такой студент есть: ");
+                            student.Print();
+                            found = true;
+                        }
+                    }
+                    
+                    if (!found) 
+                    {
+                        Console.WriteLine("Такого студента нет.");
+                    }
+                    break;
+                case 5:
+
+                    Console.Write("Введите средний бал для поиска: ");
+                    string search_average = Console.ReadLine();
+                    foreach (Student student in students)
+                    {
+                        string str_average = student.Average.ToString();    
+                        if (str_average.Equals(search_average, StringComparison.OrdinalIgnoreCase))
+                        {
+                            Console.WriteLine("Такой студент есть: ");
+                            student.Print();
+                            found = true;
+                        }
+                    }
+
+                    if (!found)
+                    {
+                        Console.WriteLine("Такого студента нет.");
+                    }
+                    break;
+                case 6:
+                    Console.Write("Введите группу для поиска: ");
+                    string search_number_of_group = Console.ReadLine();
+                    foreach (Student student in students)
+                    {   
+                        string str_number_of_group = student.NumberOfGroup.ToString();
+                        if (str_number_of_group.Equals(search_number_of_group, StringComparison.OrdinalIgnoreCase))
+                        {
+                            Console.WriteLine("Такой студент есть: ");
+                            student.Print();
+                            found = true;
+                        }
+                    }
+
+                    if (!found)
+                    {
+                        Console.WriteLine("Такого студента нет.");
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ ConsoleApp9/Class4.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class ClassMenu
+    {
+        Academy_Group academy_Group = new Academy_Group();
+        bool running = true;
+
+            
+        public void Menu() 
+        {
+            while (running)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("Выберите действие:");
+                Console.WriteLine("1. Добавить студента");
+                Console.WriteLine("2. Удалить студента");
+                Console.WriteLine("3. Редактировать студента");
+                Console.WriteLine("4. Показать группу");
+                Console.WriteLine("5. Сохранить данные");
+                Console.WriteLine("6. Загрузить данные");
+                Console.WriteLine("7. Поиск студента");
+                Console.WriteLine("0. Выход");
+                Console.Write("Ваш выбор: ");
+
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        Console.Write("Введите имя: ");
+                        string name = Console.ReadLine();
+                        Console.Write("Введите фамилию: ");
+                        string surname = Console.ReadLine();
+                        Console.Write("Введите телефон: ");
+                        string phone = Console.ReadLine();
+                        Console.Write("Введите возраст: ");
+                        int age = int.Parse(Console.ReadLine());
+                        Console.Write("Введите средний балл: ");
+                        double average = double.Parse(Console.ReadLine());
+                        Console.Write("Введите номер группы: ");
+                        int numberOfGroup = int.Parse(Console.ReadLine());
+
+                        Student student = new Student(name, surname, age, phone, average, numberOfGroup);
+                        academy_Group.Add(student);
+                        break;
+                    case "2":
+                        Console.Write("Введите фамилию студента для удаления: ");
+                        string remove_surname = Console.ReadLine();
+                        academy_Group.Remove(remove_surname);
+                        break;
+                    case "3":
+                        Console.Write("Введите фамилию студента для редактирования: ");
+                        string editSurname = Console.ReadLine();
+
+                        Console.Write("Введите новое имя: ");
+                        string newName = Console.ReadLine();
+                        Console.Write("Введите новую фамилию: ");
+                        string newSurname = Console.ReadLine();
+                        Console.Write("Введите новый телефон: ");
+                        string newPhone = Console.ReadLine();
+                        Console.Write("Введите новый возраст: ");
+                        int newAge = int.Parse(Console.ReadLine());
+                        Console.Write("Введите новый средний балл: ");
+                        double newAverage = double.Parse(Console.ReadLine());
+                        Console.Write("Введите новый номер группы: ");
+                        int newNumberOfGroup = int.Parse(Console.ReadLine());
+
+                        Student newStudent = new Student(newName, newSurname, newAge, newPhone, newAverage, newNumberOfGroup);
+                        academy_Group.Edit(editSurname, newStudent);
+                        break;
+
+                    case "4":
+                        academy_Group.Print();
+                        break;
+
+                    case "5":
+                        academy_Group.Save();
+                        break;
+
+                    case "6":
+                        academy_Group.Load();
+                        break;
+
+                    case "7":
+                        Console.WriteLine("Выберите критерий для поиска:");
+                        Console.WriteLine("1. Имя");
+                        Console.WriteLine("2. Фамилия");
+                        Console.WriteLine("3. Телефон");
+                        Console.WriteLine("4. Возраст");
+                        Console.WriteLine("5. Средний балл");
+                        Console.WriteLine("6. Номер группы");
+                        Console.Write("Ваш выбор: ");
+
+                        int search_сriterion = int.Parse(Console.ReadLine());
+                        academy_Group.Search(search_сriterion);
+                        break;
+
+                    case "0":
+                        running = false; 
+                        break;
+                }
+            }
+        }
+
+
+    }
+}
